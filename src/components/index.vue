7aea0e5ee821bb80853d0b402d98f6e4840f3efa
zy <123@qq.com>
2018-07-26 21:38:55 +0800

    slowslowindex

---
+++ src/components/index.vue
@@ -0,0 +1,157 @@
+<template>
+  
+    <div class="index">
+        <div class="section">
+            <div class="location">
+                <span>当前位置：</span>
+                <a href="#/" class="router-link-active">首页</a> &gt;
+                <a href="#/site/goodslist" class="router-link-exact-active router-link-active">购物商城</a>
+            </div>
+        </div>
+        <div class="section">
+            <div class="wrapper">
+                <div class="wrap-box">
+                    <div class="left-220" style="margin: 0px;">
+                        <div class="banner-nav">
+                            <ul>
+                                <li v-for="(item, index) in catelist" :key="item.id">
+                                    <h3>
+                                        <i class="iconfont icon-arrow-right"></i>
+                                        <span>{{item.title}}</span>
+                                        <p>
+                                            <span v-for="(itemSon, i) in item.subcates" :key="itemSon.id">
+                                                {{itemSon.title}}&nbsp;
+                                            </span>
+                                        </p>
+                                    </h3>
+                                    <div class="item-box">
+                                        <dl>
+                                            <dt>
+                                                <a href="/goods/40.html">{{item.title}}</a>
+                                            </dt>
+                                            <dd>
+                                                <a v-for="(itemSon, i) in item.subcates" :key="itemSon.id" href="/goods/43.html">{{itemSon.title}}</a>
+
+                                            </dd>
+                                        </dl>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!--幻灯片-->
+                    <div class="left-705">
+                      <!-- Swiper -->
+                           <el-carousel indicator-position="outside" class="centerImg">
+                             <el-carousel-item v-for="(item, index) in sliderlist" :key="index">
+                                 <img :src="item.img_url" alt="">
+                              </el-carousel-item>
+                           </el-carousel> 
+                    </div>
+                    <!--/幻灯片-->
+                    <div class="left-220">
+                        <ul class="side-img-list">
+                            <li v-for="(item, index) in toplist" :key="item.id">
+                                <div class="img-box">
+                                    <label>{{index+1}}</label>
+                                    <img :src="item.img_url">
+                                </div>
+                                <div class="txt-box">
+                                    <a href="/goods/show-98.html">{{item.title}}</a>
+                                    <span>{{item.add_time}}</span>
+                                </div>
+                            </li>
+            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section" v-for="(item, index) in goodList" :key="item.level1cateid">
+            <div class="main-tit">
+                <h2>{{item.catetitle}}</h2>
+                <p>
+                    <a href="/goods/43.html" v-for="(itemSon, i) in item.level2catelist">{{itemSon.subcatetitle}}</a>
+                    <a href="/goods/40.html">更多
+                        <i>+</i>
+                    </a>
+                </p>
+            </div>
+            <div class="wrapper clearfix">
+                <div class="wrap-box">
+                    <ul class="img-list">
+                        <li v-for="(itemSon, i) in item.datas" :key ="itemSon.artID">
+                            <a href="#/site/goodsinfo/87" class="">
+                                <div class="img-box">
+                                    <img :src="itemSon.img_url">
+                                </div>
+                                <div class="info">
+                                    <h3>{{itemSon.artTitle}}</h3>
+                                    <p class="price">
+                                        <b>{{itemSon.market_price}}</b>元</p>
+                                    <p>
+                                        <strong>库存 {{itemSon.stock_quantity}}</strong>
+                                        <span>市场价：
+                                            <s>{{itemSon.sell_price}}</s>
+                                        </span>
+                                    </p>
+                                </div>
+                            </a>
+                        </li>
+                
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      catelist: [],
+      sliderlist: [],
+      toplist: [],
+      goodList:[]
+    };
+  },
+  beforeMount() {
+    axios
+      .get("http://47.106.148.205:8899/site/goods/gettopdata/goods")
+      .then(response => {
+        this.catelist = response.data.message.catelist;
+        this.sliderlist = response.data.message.sliderlist;
+        this.toplist = response.data.message.toplist;
+        // console.log(this);
+      })
+      .catch(error => {
+        // console.log(error);
+      });
+
+
+      //获取底部的商品列表的数据
+      axios.get('http://47.106.148.205:8899/site/goods/getgoodsgroup')
+      .then((response)=>{
+        //   console.log(response);
+          this.goodList = response.data.message;
+              
+      })
+      .catch(response=>{
+
+      })
+  }
+};
+</script>
+<style scoped>
+@import url("../assets/statics/lib/hoverNav/css/style.css");
+
+
+.el-carousel img {
+    display: block;
+    width: 100%;
+    height: 100%;
+}
+</style>
+
+
---
+++ src/main.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import App from './App.vue'
+// 引入路由模块
+import VueRouter from 'vue-router'
+// 引入index组件
+import index from './components/index.vue'
+// 使用路由中间件
+Vue.use(VueRouter);
+//引入element ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+
+// 注册路由规则
+const router = new VueRouter({
+  routes:[
+    {path: '/', redirect:"/index"},
+    {
+      path:'/index',
+      component:index
+    }
+  ]
+})
+
+// 引入css
+import './assets/statics/site/css/style.css';
+
+Vue.config.productionTip = false
+
+new Vue({
+  // 选择器
+  el:"#app",
+  // 挂载到vue
+  router,
+  // 渲染 App组件
+  render: h => h(App)
+})
